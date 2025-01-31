80d427c4a400169173c0e40cccb5e3bd371385c2
yashash1111 <yashash.r.s70@kalvium.community>
2025-01-31 11:16:10 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to database');
+    })
+    .catch((error) => {
+        console.error(`Error connecting to database: ${error.message}`);
+    });
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
